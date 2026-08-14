5a37e6593fddbd94d5785bccb5c5e26b38fc89c0
K.S.RENU <87896770+KsRenu@users.noreply.github.com>
2026-08-14 12:45:57 +0530

    Enhance CareerOpener with additional elements and actions
    Added new elements for search functionality and improved scrolling and clicking actions.

--- Career-Search/src/main/java/org/example/CareerOpener.java
+++ Career-Search/src/main/java/org/example/CareerOpener.java
@@ -3,14 +3,18 @@ package org.example;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.tools.picocli.CommandLine;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.swing.*;
 import java.time.Duration;
 
 
@@ -33,6 +37,17 @@ public class CareerOpener{
     @FindBy(xpath = "//div[contains(text(),'Work Experience of 4')]")
     WebElement workExperienceTxt;
 
+    @FindBy(id="btn-search")
+    WebElement searchBtn;
+
+    @FindBy(css = ".sk-fading-circle.ng-star-inserted")
+    WebElement loader;
+
+    @FindBy(css="input[placeholder='Your Skills..']")
+    WebElement skills;
+
+    @FindBy(xpath = "//span[text()=' Technology->Automated Testing->Selenium-Java ']")
+    WebElement skillOption;
     public CareerOpener(WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(this.driver,this);
@@ -40,23 +55,31 @@ public class CareerOpener{
 
     public void infosys(String url){
         Logger log =    LogManager.getLogger(String.valueOf(CareerOpener.class));
+        Actions action= new Actions(driver);
         Helper helper = new Helper();
         driver.get(url);
-        //wait.until(ExpectedConditions.elementToBeClickable(experiencedProfessionals));
-        helper.wait(driver, experiencedProfessionals);
-        helper.scrollToElement(driver, experiencedProfessionals);
-        experiencedProfessionals.click();
-        helper.wait(driver, india);
-        helper.scrollToElement(driver, india);
-        india.click();
-        helper.wait(driver,searchBoxTxt);
-        helper.scrollToElement(driver, india);
-        searchBoxTxt.sendKeys("java selenium");
-        helper.wait(driver,findJobsBtn);
-        helper.scrollToElement(driver, india);
-        findJobsBtn.click();
-        helper.wait(driver,workExperienceTxt);
-        helper.scrollToElement(driver, india);
+        ((JavascriptExecutor)driver).executeScript("window.scrollBy(0,500)");
+        helper.clickElement(driver, experiencedProfessionals,"Experienced Professionals");
+
+        helper.wait(driver,searchBtn,"Search Button");
+        ((JavascriptExecutor)driver).executeScript("window.scrollBy(0,1000)");
+        helper.clickElement(driver,india,"India");
+
+        String parentWindow = driver.getWindowHandle();
+        for(String childWindow : driver.getWindowHandles()){
+            if(!parentWindow.equals(childWindow)){
+                driver.switchTo().window(childWindow);
+            }
+        }
+        helper.sendKeysToElement(driver,searchBoxTxt,"java selenium","Search Box");
+        helper.clickElement(driver,findJobsBtn,"Find Jobs Button");
+
+        action.pause(Duration.ofSeconds(5)).perform();
+        helper.sendKeysToElement(driver,skills,"Selenium-Java","Skills");
+        action.sendKeys(Keys.BACK_SPACE).perform();
+        helper.clickElement(driver,skillOption,"Technology->Automated Testing->Selenium-Java");
+
+        helper.clickElement(driver,workExperienceTxt,"Work Experience Text");
         if(workExperienceTxt.isDisplayed()){
             log.info("Available");
         }
