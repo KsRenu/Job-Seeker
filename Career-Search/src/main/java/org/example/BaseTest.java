f43629f1123c7faa927762f482529cb913de70a7
K.S.RENU <87896770+KsRenu@users.noreply.github.com>
2026-08-15 11:54:24 +0530

    Enhance BaseTest with  exception handling
    Added window maximization and exception handling to companyTest.

--- Career-Search/src/main/java/org/example/BaseTest.java
+++ Career-Search/src/main/java/org/example/BaseTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import jakarta.mail.MessagingException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,10 +14,11 @@ public class BaseTest {
     @BeforeTest
     public void setup(){
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
     
     @Test(dataProvider = "data", dataProviderClass = ExcelDataProvider.class)
-    public void companyTest(String company, String pageLink){
+    public void companyTest(String company, String pageLink) throws MessagingException {
         CareerOpener career = new CareerOpener(driver);
         career.infosys(pageLink);
     }
@@ -25,6 +27,7 @@ public class BaseTest {
     public void teardown(){
         if(driver != null){
             driver.quit();
+
         }
     }
 
